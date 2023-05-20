0c9d321aecafc50a7bbb9a89043f3deb03250504
matheusticiano <matheusticiano34@gmail.com>
2023-05-20 05:27:38 -0300

    as

--- client/api/routes/auth.route.js
+++ client/api/routes/auth.route.js
@@ -4,8 +4,12 @@ import { register, login, logout } from "../controller/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", register)
-router.post("/login", login)
+router.post("/login", (req, res) => {
+    console.log(req.cookies.accessToken);
+    login(req, res);
+  });
 router.post("/logout", logout)
+
 
 
 export default router;
