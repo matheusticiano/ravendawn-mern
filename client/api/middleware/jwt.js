74b4dab37a0cba07dc0f04e86abced847e58234f
matheusticiano <matheusticiano34@gmail.com>
2023-05-20 07:52:20 -0300

    d

--- client/api/middleware/jwt.js
+++ client/api/middleware/jwt.js
@@ -5,20 +5,13 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) return next(createError(401,"You are not authenticated!"))
 
+  console.log(token)
+
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) return next(createError(403,"Token is not valid!"))
     req.userId = payload.id;
 
-    res.cookie("accessToken", token, {
-      sameSite: "none",
-      maxAge: 3 * 24 * 60 * 60 * 1000, // 3 dias em milissegundos
-      httpOnly: true,
-      domain: "https://ravendawn.onrender.com",
-      path: "/api",
-      secure: true,
-    });
-    
     next()
   });
 };
--- client/api/routes/auth.route.js
+++ client/api/routes/auth.route.js
@@ -4,10 +4,7 @@ import { register, login, logout } from "../controller/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", register)
-router.post("/login", (req, res) => {
-    console.log(req.cookies.accessToken);
-    login(req, res);
-  });
+router.post("/login", login)
 router.post("/logout", logout)
 
 
