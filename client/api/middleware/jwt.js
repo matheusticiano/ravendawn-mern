aa886a54af36fbf75cb68f792bb48915f3aa4847
matheusticiano <matheusticiano34@gmail.com>
2023-05-20 08:04:01 -0300

    a

--- client/api/middleware/jwt.js
+++ client/api/middleware/jwt.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) return next(createError(401,"You are not authenticated!"))
 
-  console.log(token)
+  res.send(token)
 
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
